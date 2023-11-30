e1258be344104bf32d780b62409e3348e54eb324
Anushka16July <87813443+Anushka16July@users.noreply.github.com>
2023-11-30 18:47:20 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,12 +1,33 @@
 const socket = io("/");
-
-var peer = new Peer(undefined, {
-    path: "/peerjs",
+const peer = new PeerServer(undefined, {
+    path : "/peerjs",
     host: "/",
-    port: "443",
-});
+    port : "443"
+})
 
-const user = prompt("Enter your name");
+const user = prompt("Enter your name: ");
+
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+var myStream;
+
+navigator.mediaDevices.getUserMedia({
+    audio:true,
+    video:true
+}).then((stream)=>{
+    mystream = stream;
+    addVideoStream(myVideo,stream);
+})
+
+function addVideoStream(video,stream){
+   video.srcObject = stream;
+   video.addEventListener("loadedmetadata", ()=>{
+        video.play();
+       $("#video_grid").append(video);
+   })
+}
+
 
 $(function () {
     $("#show_chat").click(function () {
@@ -19,33 +40,30 @@ $(function () {
         $(".right-window").css("display", "none")
         $(".header_back").css("display", "none")
     })
-
-    $("#send").click(function () {
-        if ($("#chat_message").val().length !== 0) {
-            socket.emit("message", $("#chat_message").val());
-            $("#chat_message").val("");
+    $("#send").click(function() {
+        if($("#chat_message").val().length !==0){
+           socket.emit("message", $("#chat_message").val());
+           $("#chat_message").val() = ""
         }
     })
-
-    $("#chat_message").keydown(function (e) {
-        if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
+    $("#chat_message").keydown(function(e){
+        if(e.key == "Enter" && $("#chat_message").val().length !==0){
             socket.emit("message", $("#chat_message").val());
-            $("#chat_message").val("");
+            $("#chat_message").val() = ""
         }
     })
+}) 
+
+peer.on("open", (id)=>{
+    socket.emit("join-room", ROOM_ID, id,user);
 
 })
 
-peer.on("open", (id) => {
-    socket.emit("join-room", ROOM_ID, id, user);
-});
-
-socket.on("createMessage", (message, userName) => {
+socket.on("createMessage", (message)=>{
     $(".messages").append(`
-        <div class="message">
-            <b><i class="far fa-user-circle"></i> <span> ${userName === user ? "me" : userName
-        }</span> </b>
-            <span>${message}</span>
-        </div>
+    <div class='message'> 
+      <b><i class="far fa-user-circle"></i> <span>${userName === user ? "me" : userName}</span></b>
+      <span>${message}</span>
+    </div>
     `)
-});
+})
